114416832143df462de80bab5b5576271c13621e
Oneneobari Obe <obe.onene@gmail.com>
2022-10-21 13:38:57 +0100

    OOp- Data Abstraction

---
+++ Task23.py
@@ -0,0 +1,30 @@
+# Lesson 23 Task
+#
+# Create a class called User
+#
+# Create a private attribute called __password with the value "password" in the User class
+#
+# Create a method get_password() that returns self.__password in the User class
+#
+# Create another class called ActiveUser that inherits from the User class
+#
+# Create a method get_password() that returns "********" in the ActiveUser class
+#
+# Instantiate an object of the ActiveUser class
+#
+# Print the value of get_password() from the object instance
+
+class User:
+    __password = "password"
+
+    def get_password(self):
+        print("\nPassword:", self.__password)
+
+
+class ActiveUser(User):
+    def get_password(self):
+        print("********")
+
+
+activeUser = ActiveUser()
+activeUser.get_password()
