268d3db1f15aad1562915b63f29685590e939b6b
Oneneobari Obe <obe.onene@gmail.com>
2022-10-21 05:08:12 +0100

    Basic Dictionary Operations

---
+++ Task17.py
@@ -0,0 +1,19 @@
+# Lesson 17 Task
+#
+# Create a class called Human
+#
+# Initialize the class
+
+class Human:
+    name = "Onene"
+    group = "Vetebra"
+
+
+#def name_person(me):
+#    return me.name + ":" + me.group
+
+
+person = Human()
+
+
+print(Human.name)
